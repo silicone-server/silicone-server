de903dc8e4c0381ced752ab01b6c8c3522e059b6
Just-a-Unity-Dev <rnmangunay@addu.edu.ph>
2022-04-28 20:56:23 +0800

    discord redirect

---
+++ src/routes/discord.svelte
@@ -0,0 +1,19 @@
+<script context="module">
+	let url = "https://discord.gg/aBTkpvdZAU";
+
+	export async function load() {
+		return {
+			status: 302,
+			redirect: url
+		}
+	}
+</script>
+
+<svelte:head>
+	<title>discord</title>
+	<meta name="description" content="Discord redirect" />
+</svelte:head>
+
+<div class="content">
+	<h1>you are being redirected to our <a href={url}>discord</a></h1>
+</div>
